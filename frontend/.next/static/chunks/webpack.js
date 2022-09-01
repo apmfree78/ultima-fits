6beddab9cf465385eebeecd8177644f01301be17
apmfree78 <amit@profitswami.com>
2022-09-01 19:51:03 -0400

    fixed package.json package versions

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d946644e79e7a9ad42d4"; }
+/******/ 		__webpack_require__.h = function() { return "ccc4c00b6eb51b40a5d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
